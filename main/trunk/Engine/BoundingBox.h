2dde25068dbc4968d40f46c5377e19bebc11d35f
amd3500 <amd3500@inbox.ru>
2012-02-12 21:01:20 +0000

    + Created BoundingBox class. For now using it for debug QuadTree nodes rendering. - Try to investigate how can process of node creation be optimized and work faster. When node triangle quantity = 1000, node creation takes more than 7 seconds.

---
+++ main/trunk/Engine/BoundingBox.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <d3d11.h>
+#include <d3dx10math.h>
+
+class BoundingBox
+{
+	struct VertexTypeLine
+	{
+		D3DXVECTOR3 position;
+	};
+
+	public:
+		void CreateBBBuffers(D3DXVECTOR3 centerPos, float sizeX, float sizeY, float sizeZ, ID3D11Device* device);
+		void RenderBBBuffers(ID3D11DeviceContext* deviceContext);
+		BoundingBox(void);
+		~BoundingBox(void);
+	private:
+		ID3D11Buffer *mVertexBuffer, *mIndexBuffer;
+};
+
